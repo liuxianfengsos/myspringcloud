5727f8e6def0ba5d24d5d5a6dab7b60c7a342707
勇敢的雪球 <wy03043355@163.com>
2020-07-30 19:04:54 +0800

    gateway openfeign 01

--- myspringcloud-srpingcloudgateway-9529/src/main/java/com/mine/springcloud/myspringcloudsrpingcloudgateway9529/MyspringcloudSrpingcloudgateway9529Application.java
+++ myspringcloud-srpingcloudgateway-9529/src/main/java/com/mine/springcloud/myspringcloudsrpingcloudgateway9529/MyspringcloudSrpingcloudgateway9529Application.java
@@ -2,12 +2,13 @@ package com.mine.springcloud.myspringcloudsrpingcloudgateway9529;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class MyspringcloudSrpingcloudgateway9529Application {
-//测试时访问http://localhost:9529/hello?url=baidu 由于百度没有hello组件所以会报错误页面
+//测试时访问http://localhost:9529/dept/get/1    http://localhost:8001/dept/get/1
+//测试时访问http://localhost:9529/dept/list    http://localhost:8001/dept/get/1
     public static void main(String[] args) {
         SpringApplication.run(MyspringcloudSrpingcloudgateway9529Application.class, args);
     }
